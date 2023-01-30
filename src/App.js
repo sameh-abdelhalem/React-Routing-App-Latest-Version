24833de2a095dfd1f2a38d2e452066bb412993a2
Sameh <sameh.abdelhaleem95@gmail.com>
2023-01-30 21:16:31 +0200

    trigger a loader without loading the newsletter page using useFetcher

--- src/App.js
+++ src/App.js
@@ -1,88 +1,69 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import EventsLayout from "./components/EventsLayout";
-import RootLayout from "./components/Root";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import EditEventPage from "./pages/EditEvent";
-import EditEventPaget from "./pages/EditEvent";
 import ErrorPage from "./pages/Error";
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from "./pages/EventDetail";
-import EventPage, { loader as eventsLoader } from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventsLayout from "./pages/EventsLayout";
 import HomePage from "./pages/Home";
 import NewEventPage from "./pages/NewEvent";
-
+import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "events",
+        element: <EventsLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ":eventId",
+            id: "event-detail",
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+          {
+            path: "new",
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: "newsletter",
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          index: true,
-          element: <HomePage />,
-        },
-        {
-          path: "/events",
-          element: <EventsLayout />,
-          children: [
-            {
-              index: true,
-              element: <EventPage />,
-              loader: eventsLoader,
-            },
-            {
-              path: ":eventId",
-              id: "event-detail",
-              loader: eventDetailLoader,
-              children: [
-                {
-                  index: true,
-                  element: <EventDetailPage />,
-                  action: deleteEventAction,
-                },
-                {
-                  path: "edit",
-                  element: <EditEventPage />,
-                  action: manipulateEventAction,
-                },
-              ],
-            },
-            {
-              path: "/events/new",
-              element: <NewEventPage />,
-              action: manipulateEventAction,
-            },
-          ],
-        },
-      ],
-    },
-  ]);
   return <RouterProvider router={router} />;
 }
 
